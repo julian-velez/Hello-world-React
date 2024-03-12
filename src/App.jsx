9fc181cdd0e7c45c20dbd7c188e7dd0c2fa8b58a
julian <juliansolo7189@gmail.com>
2024-03-12 08:17:21 -0500

    Hello world React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function Micomponente() {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <p>este es un componente funcional</p>
+    </div>
+  )
+}
+
+export default Micomponente;
